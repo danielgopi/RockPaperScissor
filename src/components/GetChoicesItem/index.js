d9c66659cdc0058a3561486b5ddf17aa74579d9e
danielgopi <balewayne4353@gmail.com>
2024-06-12 13:19:50 +0000

    Solution

---
+++ src/components/GetChoicesItem/index.js
@@ -0,0 +1,26 @@
+import {ChoiceItemContainer, ChoiceButton, ButtonImage} from './styledComponent'
+
+const GetChoicesItem = props => {
+  const {details, onSelectedButton} = props
+  const {id, imageUrl} = details
+
+  const testids = id.toLowerCase()
+  const testIdName = `${testids}Button`
+
+  const buttonClicked = () => {
+    onSelectedButton(id)
+  }
+
+  return (
+    <ChoiceItemContainer>
+      <ChoiceButton
+        data-testid={testIdName}
+        onClick={buttonClicked}
+        type="button"
+      >
+        <ButtonImage alt={id} src={imageUrl} />
+      </ChoiceButton>
+    </ChoiceItemContainer>
+  )
+}
+export default GetChoicesItem
